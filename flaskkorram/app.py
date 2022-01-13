8d21dffad2f3fc9ca9a1aa58ce19a41dbb967a70
Amanur Rahman <aaman.sunny007@gmail.com>
2022-01-14 00:51:08 +0600

    Configured multiple flask app with DispatchedMiddleware

---
+++ flaskkorram/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+from .bookly.app import app as bookly_app
+from .productly.app import app as productly_app
+
+
+app = Flask(__name__)
+
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/books': bookly_app,
+    '/products': productly_app
+})
+
+@app.route('/')
+def hello():
+    return 'Welcome to Home Page'
+
+
